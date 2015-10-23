cbe10eabdbfbf52553ceb2ac6f3e840eaf6617f1
Richard C Gerkin <rgerkin@asu.edu>
2015-10-22 17:23:42 -0700

    Added common configuration script for running notebooks

--- suites/configure.py
+++ suites/configure.py
@@ -1,15 +1,24 @@
 import os
 import sys
 
+# Use *'s in path names to prepend instead of append them to the Python path.  
+
 paths = {}
 # Path to this repository's top directory (containing models, tests, etc.)
-paths['SUITE_REPO'] = os.path.expand('../..')
+paths['SUITE_REPO'] = os.path.abspath('../..')
 # Path to NEURON python libraries.  
 paths['NEURON_PYTHON_PATH'] = '/Applications/NEURON/nrn/lib/python'
-# Path to development version of NeuronUnit.
-paths['NEURONUNIT_DEV_PATH'] = '/Users/rgerkin/Dropbox/dev/scidash/neuronunit'
 
-for path in paths:
+# Path to development version of NeuronUnit.
+paths['SCIUNIT_DEV_PATH*'] = '/Users/rgerkin/Dropbox/dev/scidash/sciunit'
+
+# Path to development version of NeuronUnit.
+paths['NEURONUNIT_DEV_PATH*'] = '/Users/rgerkin/Dropbox/dev/scidash/neuronunit'
+
+for path_name,path in paths.items():
     if path not in sys.path:
-        sys.path.append(path)
+        if '*' in path_name:
+            sys.path.insert(1,path)
+        else:
+            sys.path.append(path)
 
